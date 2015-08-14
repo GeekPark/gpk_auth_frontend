a753454da6ddfc26a5e6399bded12c780426053a
Vanilla <mail@liyaodong.com>
2015-08-14 14:16:19 +0800

    change wepack-dev-server into server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+var webpack = require('webpack');
+var WebpackDevServer = require('webpack-dev-server');
+var config = require('./webpack.hot');
+
+var server = new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true,
+  noInfo: false,
+  stats: {colors: true}
+});
+
+console.log('webpack-dev-server was running in localhost:8080');
+
+server.listen(8080, 'localhost', function() {});
