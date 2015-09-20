879d7d26b632a81b3dda94f38fee895aa319585a
Vanilla <mail@liyaodong.com>
2015-09-20 18:46:21 +0800

    add webpack proxy to rails

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 GeekPark auth frontend
 ---------------------
+### Start Dev
+
+#### 1. start rails server on `localhost:3000`
+#### 2. npm start will start webpack server in `localhost:8080`
+#### 3. visit `localhost:8000` webpack server will proxy to rails 3000 server
--- server.js
+++ server.js
@@ -6,7 +6,10 @@ var server = new WebpackDevServer(webpack(config), {
   publicPath: config.output.publicPath,
   hot: true,
   noInfo: false,
-  stats: { colors: true }
+  stats: { colors: true },
+  proxy: {
+    '*': 'http://localhost:3000'
+  }
 });
 
 console.log('webpack-dev-server was running in localhost:8080');
